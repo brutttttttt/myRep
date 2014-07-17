664cc8469c0aff8710051f1a8fe4d34a132da6ac
Sergey_Petrychenko <Sergey_Petrychenko@epam.com>
2014-07-17 15:54:25 +0300

    trttgggggghhh

--- webinar-services/src/main/java/com/epam/cq/demo/services/GoodbyeWorldServiceImpl.java
+++ webinar-services/src/main/java/com/epam/cq/demo/services/GoodbyeWorldServiceImpl.java
@@ -6,7 +6,7 @@ import org.apache.felix.scr.annotations.*;
 import org.osgi.framework.Constants;
 
 /**
- * Example OSGi service using SCR annotations.fghfghgfghfhfh
+ * Example OSGi service using SCR annotations.
  */
 @Component(immediate = true, metatype = true)
 @Service(GoodbyeWorldService.class)
